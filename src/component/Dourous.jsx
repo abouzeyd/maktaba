6098ce1a2bdfaa88ff8dc1fc3199acff341152a9
mohou <mohoumouhamed01@gmail.com>
2021-09-16 17:25:17 +0000

    feat:news features

--- src/component/Dourous.jsx
+++ src/component/Dourous.jsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import audio from '../assets/microphone-solid.svg';
+import conference from '../assets/headphones-solid.svg';
+import preche from '../assets/think-peaks-brands.svg';
 import dourous from '../data/dourous.json';
 import conferences from '../data/conferences.json';
 import preches from '../data/prêches.json';
 
 const Dourous = () => {
   return (
-    <div style={{ margin: 'auto' }}>
+    <div>
       <div
         style={{
-          maxHeight: 300,
-          overflow: 'scroll',
-          width: '20%',
-          backgroundColor: '#c25542',
-          borderRadius: '6px',
-          border: '3px solid #c25542',
-          margin: 'auto',
-          padding: 7,
-          color: 'white',
-          textAlign: 'center',
           display: 'flex',
+          margin: 'auto',
+          width: '50%',
           alignItems: 'center',
+          justifyContent: 'space-between',
         }}
       >
-        <img src={audio} alt="h" width="15px" style={{ marginLeft: 35 }} />
-        <span style={{ marginLeft: 18 }}> Cours Audio</span>
+        <div
+          style={{
+            maxHeight: 300,
+            overflow: 'scroll',
+            backgroundColor: '#c25542',
+            borderRadius: '6px',
+            border: '3px solid #c25542',
+            padding: 7,
+            color: 'white',
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <img
+            src={conference}
+            alt="h"
+            width="20px"
+            style={{ marginLeft: 35 }}
+          />
+          <span style={{ marginLeft: 18 }}> Cours Audio</span>
+        </div>
+        <div style={{ fontSize: 23, color: '#ababab', fontWeight: 'bold' }}>
+          Nouveautés
+        </div>
       </div>
 
       <div
@@ -45,7 +63,13 @@ const Dourous = () => {
         <div>
           {dourous.map((item, index) => {
             return (
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  borderBottom: '1px solid black',
+                }}
+              >
                 <div
                   style={{
                     display: 'flex',
@@ -53,10 +77,23 @@ const Dourous = () => {
                     justifyContent: 'space-between',
                   }}
                 >
-                  <h4>{item.name}</h4>
+                  <h4
+                    style={{
+                      backgroundColor: '#ababab',
+                      border: '3px solid  #fff',
+                      borderRadius: 6,
+                      color: 'white',
+                      padding: 12,
+                    }}
+                  >
+                    {item.name}
+                  </h4>
                   <span style={{ fontSize: 14 }}>{item.date}</span>
                 </div>
-                <a href="/" style={{ textDecoration: 'none' }}>
+                <a
+                  href="/"
+                  style={{ textDecoration: 'none', marginBottom: 12 }}
+                >
                   {item.title}
                 </a>
               </div>
@@ -67,23 +104,34 @@ const Dourous = () => {
       {/* conferences */}
       <div
         style={{
-          maxHeight: 300,
-          overflow: 'scroll',
-          width: '20%',
-          backgroundColor: '#65abc2',
-          borderRadius: '6px',
-          border: '3px solid #65abc2',
-          margin: 'auto',
-          padding: 7,
-          color: 'white',
-          textAlign: 'center',
           display: 'flex',
           alignItems: 'center',
+          margin: 'auto',
+          width: '50%',
+          justifyContent: 'space-between',
           marginTop: 25,
         }}
       >
-        <img src={audio} alt="h" width="15px" style={{ marginLeft: 35 }} />
-        <span style={{ marginLeft: 18 }}> Conférences</span>
+        <div
+          style={{
+            maxHeight: 300,
+            overflow: 'scroll',
+            backgroundColor: '#65abc2',
+            borderRadius: '6px',
+            border: '3px solid #65abc2',
+            padding: 2,
+            color: 'white',
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <img src={audio} alt="h" width="20px" style={{ marginLeft: 35 }} />
+          <span style={{ marginLeft: 18 }}> Conférences</span>
+        </div>
+        <div style={{ fontSize: 23, color: '#ababab', fontWeight: 'bold' }}>
+          Nouveautés
+        </div>
       </div>
 
       <div
@@ -104,7 +152,13 @@ const Dourous = () => {
         <div>
           {conferences.map((item, index) => {
             return (
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  borderBottom: '1px solid black',
+                }}
+              >
                 <div
                   style={{
                     display: 'flex',
@@ -112,10 +166,23 @@ const Dourous = () => {
                     justifyContent: 'space-between',
                   }}
                 >
-                  <h4>{item.name}</h4>
+                  <h4
+                    style={{
+                      backgroundColor: '#ababab',
+                      border: '3px solid  #fff',
+                      borderRadius: 6,
+                      color: 'white',
+                      padding: 12,
+                    }}
+                  >
+                    {item.name}
+                  </h4>
                   <span style={{ fontSize: 14 }}>{item.date}</span>
                 </div>
-                <a href="/" style={{ textDecoration: 'none' }}>
+                <a
+                  href="/"
+                  style={{ textDecoration: 'none', marginBottom: 12 }}
+                >
                   {item.title}
                 </a>
               </div>
@@ -126,23 +193,42 @@ const Dourous = () => {
       {/* Prêches */}
       <div
         style={{
-          maxHeight: 300,
-          overflow: 'scroll',
-          width: '20%',
-          backgroundColor: '#84b856',
-          borderRadius: '6px',
-          border: '3px solid #84b856',
-          margin: 'auto',
-          padding: 7,
-          color: 'white',
-          textAlign: 'center',
           display: 'flex',
           alignItems: 'center',
+          margin: 'auto',
+          width: '50%',
+          justifyContent: 'space-between',
           marginTop: 25,
         }}
       >
-        <img src={audio} alt="h" width="15px" style={{ marginLeft: 35 }} />
-        <span style={{ marginLeft: 18 }}> Prêches</span>
+        <div
+          style={{
+            maxHeight: 300,
+            overflow: 'scroll',
+            backgroundColor: '#84b856',
+            borderRadius: '6px',
+            border: '3px solid #84b856',
+            padding: 7,
+            color: 'white',
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            marginTop: 25,
+          }}
+        >
+          <img src={preche} alt="h" width="20px" style={{ marginLeft: 35 }} />
+          <span style={{ marginLeft: 18 }}> Prêches</span>
+        </div>
+        <div
+          style={{
+            fontSize: 23,
+            color: '#ababab',
+            fontWeight: 'bold',
+            marginTop: 25,
+          }}
+        >
+          Nouveautés
+        </div>
       </div>
 
       <div
@@ -163,7 +249,13 @@ const Dourous = () => {
         <div>
           {preches.map((item, index) => {
             return (
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  borderBottom: '1px solid black',
+                }}
+              >
                 <div
                   style={{
                     display: 'flex',
@@ -171,10 +263,23 @@ const Dourous = () => {
                     justifyContent: 'space-between',
                   }}
                 >
-                  <h4>{item.name}</h4>
+                  <h4
+                    style={{
+                      backgroundColor: '#ababab',
+                      border: '3px solid  #fff',
+                      borderRadius: 6,
+                      color: 'white',
+                      padding: 12,
+                    }}
+                  >
+                    {item.name}
+                  </h4>
                   <span style={{ fontSize: 14 }}>{item.date}</span>
                 </div>
-                <a href="/" style={{ textDecoration: 'none' }}>
+                <a
+                  href="/"
+                  style={{ textDecoration: 'none', marginBottom: 12 }}
+                >
                   {item.title}
                 </a>
               </div>
